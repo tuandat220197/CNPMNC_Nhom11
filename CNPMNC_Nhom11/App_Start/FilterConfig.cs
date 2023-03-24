f2a53f835e3c140b139deedb4087e4893502812a
TuanDat <tuandat22011997@gmail.com>
2023-03-24 21:00:03 +0700

    Add project files.

---
+++ CNPMNC_Nhom11/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CNPMNC_Nhom11
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CNPMNC_Nhom11/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CNPMNC_Nhom11.MvcApplication" Language="C#" %>
